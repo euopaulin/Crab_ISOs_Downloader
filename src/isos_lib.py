e28f2fd1ade4aae9be8f845a1f78047663df6180
Paulo Henrique <paulohspfc209@gmail.com>
2025-09-05 10:29:23 -0300

    Criado uma lib com as isos

---
+++ src/isos_lib.py
@@ -0,0 +1,18 @@
+isos_disponiveis = {
+    "ubuntu Desktop": {
+        "nome_exibicao": "Ubuntu Desktop 24.04 LTS",
+        "url":
+        "https://ubuntu.com/download/desktop/thank-you?version=24.04.3&architecture=amd64&lts=true",
+        "nome_arquivo": "ubuntu-24.04-desktop-amd64.iso"
+    },
+    "Windows 11": {
+        "nome_exibicao": "Windows 11",
+        "url": "https://www.microsoft.com/pt-br/software-download/windows11",
+        "nome_arquivo": "windows11.iso"
+    },
+    "Windows 10": {
+        "nome_exibicao": "Windows 10",
+        "url": "https://www.microsoft.com/pt-br/software-download/windows10",
+        "nome_arquivo": "windows10.iso"
+    },
+}
--- src/main.py
+++ src/main.py
@@ -1,33 +1,24 @@
 import requests
 import os
+import isos_lib
 
-isos_disponiveis = {
-    "ubuntu Desktop": {
-        "nome_exibicao": "Ubuntu Desktop 24.04 LTS",
-        "url":
-        "https://ubuntu.com/download/desktop/thank-you?version=24.04.3&architecture=amd64&lts=true",
-        "nome_arquivo": "ubuntu-24.04-desktop-amd64.iso"
-    },
-    "Windows 11": {
-        "nome_exibicao": "Windows 11",
-        "url": "https://www.microsoft.com/pt-br/software-download/windows11",
-        "nome_arquivo": "windows11.iso"
-    },
-}
-
+#Esse aqui vai listar as isos
+isos_disponiveis = isos_lib.isos_disponiveis
 nome_exibicao = "\n".join(
-    [iso["nome_exibicao"] for iso in isos_disponiveis.values()])
+        [iso["nome_exibicao"] for iso in isos_disponiveis.values()])
 print(nome_exibicao)
 iso_selecionada = input("Digite o nome do ISO que deseja baixar: ")
 nome_exibicao = iso_selecionada
 iso_selecionada = isos_disponiveis.get(nome_exibicao)
 
 if iso_selecionada is None:
-    print("Nenhum ISO selecionado.")
+        print("Nenhum ISO selecionado ou nome digitado errado." 
+              "\n" "Tente novamente.")
+        
 else:
-    print("ISO selecionado:", iso_selecionada["nome_exibicao"])
+        print("ISO selecionado:", iso_selecionada["nome_exibicao"])
 
-
+#Esse aqui vai fazer o download da iso
 def download_iso():
     try:
         resposta = requests.get(iso_selecionada["url"], stream=True)
@@ -39,9 +30,6 @@ def download_iso():
         print("Requisição bem-sucedida!")
     else:
         print("Erro na requisição:", resposta.status_code)
-        # Verifica se houve algum erro na requisição
-
-    #segunda parte
 
     tamanho_total = float(resposta.headers.get('content-length', 0))
     tamanho_total_mb = tamanho_total / (1024 * 1024)
@@ -65,6 +53,4 @@ def download_iso():
     except requests.exceptions.RequestException as erro2:
         print("Erro ao fazer o download:", erro2)
         return
-
-
 download_iso()
