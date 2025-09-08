1133f7b478ddaf9c6499c3f0299491151540aefc
Paulo Henrique <paulohspfc209@gmail.com>
2025-09-08 11:18:10 -0300

    Atualizando código

--- src/isos_lib.py
+++ src/isos_lib.py
@@ -15,4 +15,9 @@ isos_disponiveis = {
         "url": "https://software.download.prss.microsoft.com/dbazure/Win10_22H2_BrazilianPortuguese_x64v1.iso?t=405dc03b-6a29-4852-ac1b-5a62b3785afc&P1=1757366713&P2=601&P3=2&P4=kiaRiSmmONsmiGNgcuXuCsHqAuPjic0wQzizv9FcpmYEPeduayKpfIBHxmexdV81MuI2cyP4jeRogZDTiNd7uvEbPMaXT%2bZNhvtb1MhnaI6kYiNP90NjiQcvC%2f5Ws2blqbc021lBGU1VhbZ81TRRuuzWf6Kz6VwCT%2bvQ%2fkbBuiGjX5EUL0%2bvXazJ4knDhkIovk7tPmKZP3cWE%2fNCeyNc4q1%2fBQvpLdg6UmhZ5ToU%2bHWKTSPR6WQ30roKpLvFf%2baLU0O%2b9sLB%2fX0L4wwqTMYUELzKyN1FNo5cJtl%2bD68SCyWaBIsJh7or79od6qrgkYXVFdaZMIEYsbJ3dXT46JgvAw%3d%3d",
         "nome_arquivo": "windows10.iso"
     },
+    "Windows 7": {
+        "nome_exibicao": "Windows 7",
+        "url": "https://archive.org/download/w-7-prolite-4.0x-64.x-86/W7PROLITE4.0x64.x86.iso",
+        "nome_arquivo": "windows7.iso"
+    },   
 }
