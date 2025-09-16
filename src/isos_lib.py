d53dd8bc2802750562d358d9a14b3f2d354cfcc8
Paulo Henrique <paulohspfc209@gmail.com>
2025-09-16 09:59:00 -0300

    Atualização do código e implementado interface gráfica

--- src/isos_lib.py
+++ src/isos_lib.py
@@ -1,8 +1,8 @@
 isos_disponiveis = {
     "ubuntu Desktop": {
-        "nome_exibicao": "Ubuntu",
+        "nome_exibicao": "Ubuntu 24.04",
         "url":
-        "https://releases.ubuntu.com/24.04.3/ubuntu-24.04.3-desktop-amd64.iso?_gl=1*1krwy3w*_gcl_au*OTQyNTIxNjYwLjE3NTM5NjA4NzQ.",
+        "https://releases.ubuntu.com/24.04.3/ubuntu-24.04.3-desktop-amd64.iso?_gl=1*h1t7on*_gcl_au*OTQyNTIxNjYwLjE3NTM5NjA4NzQ.",
         "nome_arquivo": "ubuntu-24.04.iso"
     },
     "Windows 11": {
@@ -12,7 +12,7 @@ isos_disponiveis = {
     },
     "Windows 10": {
         "nome_exibicao": "Windows 10",
-        "url": "https://software.download.prss.microsoft.com/dbazure/Win10_22H2_BrazilianPortuguese_x64v1.iso?t=405dc03b-6a29-4852-ac1b-5a62b3785afc&P1=1757366713&P2=601&P3=2&P4=kiaRiSmmONsmiGNgcuXuCsHqAuPjic0wQzizv9FcpmYEPeduayKpfIBHxmexdV81MuI2cyP4jeRogZDTiNd7uvEbPMaXT%2bZNhvtb1MhnaI6kYiNP90NjiQcvC%2f5Ws2blqbc021lBGU1VhbZ81TRRuuzWf6Kz6VwCT%2bvQ%2fkbBuiGjX5EUL0%2bvXazJ4knDhkIovk7tPmKZP3cWE%2fNCeyNc4q1%2fBQvpLdg6UmhZ5ToU%2bHWKTSPR6WQ30roKpLvFf%2baLU0O%2b9sLB%2fX0L4wwqTMYUELzKyN1FNo5cJtl%2bD68SCyWaBIsJh7or79od6qrgkYXVFdaZMIEYsbJ3dXT46JgvAw%3d%3d",
+        "url": "https://delivery.activated.win/dbmassgrave/pt-br_windows_10_consumer_editions_version_22h2_updated_aug_2025_x64_dvd_65bb5415.iso",
         "nome_arquivo": "windows10.iso"
     },
     "Windows 7": {
